943daddfa4f26d709aab904ffc24aea4d3ad07d2
Lavabre <jle@eau-adour-garonne.fr>
2016-10-12 12:30:51 +0200

    12/10/2016 : avancement hydro et analyses

--- src/Aeag/SqeBundle/Controller/ProgrammationAvancementController.php
+++ src/Aeag/SqeBundle/Controller/ProgrammationAvancementController.php
@@ -21,7 +21,7 @@ class ProgrammationAvancementController extends Controller {
         $session->set('menu', 'programmation');
         $session->set('controller', 'ProgrammationAvancement');
         $session->set('fonction', 'hydroIndex');
-           
+
         return $this->render('AeagSqeBundle:Programmation:Avancement\hydroIndex.html.twig');
     }
 
@@ -37,89 +37,21 @@ class ProgrammationAvancementController extends Controller {
         $session->set('fonction', 'hydroGlobal');
         $em = $this->get('doctrine')->getManager();
         $emSqe = $this->get('doctrine')->getManager('sqe');
-        
-        $repoPgProgMarche =$emSqe->getRepository('AeagSqeBundle:PgProgMarche');
 
-        $tableau = array();
-        $tableau= $repoPgProgMarche->getAvancementGlobal1();
-        //$tableau1 = $repoPgProgMarche->getAvancementGlobal();
-        $tableau1 = array();
-       
-        $tabMarche = array();
-        $j = 0;
-        for ($i = 0; $i < count($tableau); $i++){
-            $trouve = false;
-            for ($j = 0; $j < count($tabMarche);$j++){
-                if ($tabMarche[$j] == $tableau[$i]['typeMarche']){
-                    $trouve = true;
-                    break;
-                }
-            }
-            if (!$trouve){
-                $j = count($tabMarche);
-                $tabMarche[$j] = $tableau[$i]['typeMarche'];
-              }
-        }
-        
-        $tabStatut = array();
-        $j = 0;
-        for ($i = 0; $i < count($tableau); $i++){
-            $trouve = false;
-            for ($j=0; $j < count($tabStatut);$j++){
-                if ($tabStatut[$j]['code'] == $tableau[$i]['statutPrel']){
-                    $trouve = true;
-                    break;
-                }
-            }
-            if (!$trouve){
-                $j = count($tabStatut) ;
-                $tabStatut[$j]['code'] = $tableau[$i]['statutPrel'];
-                if ($tableau[$i]['statutPrel'] == 'F'){
-                    $tabStatut[$j]['libelle'] = 'Effectué sans FT';
-                }elseif ($tableau[$i]['statutPrel'] == 'D'){
-                    $tabStatut[$j]['libelle'] = 'Effectué avec FT';
-                }elseif ($tableau[$i]['statutPrel'] == 'N'){
-                    $tabStatut[$j]['libelle'] = 'Non effectué';
-                }elseif ($tableau[$i]['statutPrel'] == 'P'){
-                    $tabStatut[$j]['libelle'] = 'Prévisionnel';
-                }elseif ($tableau[$i]['statutPrel'] == 'R'){
-                    $tabStatut[$j]['libelle'] = 'Reporté';
-                }else{
-                    $tabStatut[$j]['libelle'] = 'Aucun suivi';
-                }
-            }
-        }
-        
-        $tableau3 = array();
-        $l = 0;
-        for ($i = 0; $i < count($tabStatut); $i++){
-            for($j = 0; $j < count($tabMarche); $j++){
-                for ($k = 0; $k < count($tableau); $k++){
-                     if ($tabStatut[$i]['code'] == $tableau[$k]['statutPrel']){
-                               $tableau3[$l]['statut'] = $tabStatut[$i]['libelle'];
-                              $tableau3[$l]['marche'] = $tabMarche[$j];
-                              $tableau3[$l][$j]['nbPrel'] = $tableau[$k]['nb_prel'];
-                              $l++;
-                      }
-                }
-            }
-        }
-        
-        $tableau2 = $repoPgProgMarche->getAvancementGlobal2();
-       
-       
+        $repoPgProgMarche = $emSqe->getRepository('AeagSqeBundle:PgProgMarche');
+
+        $tableau = $repoPgProgMarche->getAvancementHydrobioGlobal();
+
+
 //          \Symfony\Component\VarDumper\VarDumper::dump($tableau);
 //          return new Response (''); 
-        
+
         return $this->render('AeagSqeBundle:Programmation:Avancement\hydroGlobal.html.twig', array(
-                                      'tableau' => $tableau,
-                                      'tableau2' => $tableau2,
-                                     'tableau3' => $tableau3,
-                                      'marches' => $tabMarche,
-                                      'statuts' => $tabStatut));
+                    'tableau' => $tableau,
+        ));
     }
-    
-     public function hydroSupportAction() {
+
+    public function hydroSupportAction() {
 
         $user = $this->getUser();
         if (!$user) {
@@ -132,13 +64,15 @@ class ProgrammationAvancementController extends Controller {
         $em = $this->get('doctrine')->getManager();
         $emSqe = $this->get('doctrine')->getManager('sqe');
 
-        $tableau = array();
-        
+        $repoPgProgMarche = $emSqe->getRepository('AeagSqeBundle:PgProgMarche');
+
+        $tableau = $repoPgProgMarche->getAvancementHydrobioSupport();
+
         return $this->render('AeagSqeBundle:Programmation:Avancement\hydroSupport.html.twig', array(
-                                      'tableau' => $tableau));
+                    'tableau' => $tableau));
     }
-    
-      public function hydroLotAction() {
+
+    public function hydroLotAction() {
 
         $user = $this->getUser();
         if (!$user) {
@@ -151,11 +85,104 @@ class ProgrammationAvancementController extends Controller {
         $em = $this->get('doctrine')->getManager();
         $emSqe = $this->get('doctrine')->getManager('sqe');
 
-        $tableau = array();
-        
+        $repoPgProgMarche = $emSqe->getRepository('AeagSqeBundle:PgProgMarche');
+
+        $tableau = $repoPgProgMarche->getAvancementHydrobioLot();
+
         return $this->render('AeagSqeBundle:Programmation:Avancement\hydroLot.html.twig', array(
-                                      'tableau' => $tableau));
+                    'tableau' => $tableau));
     }
 
-  
+    public function analyseIndexAction() {
+
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->render('AeagSqeBundle:Default:interdit.html.twig');
+        }
+        $session = $this->get('session');
+        $session->set('menu', 'programmation');
+        $session->set('controller', 'ProgrammationAvancement');
+        $session->set('fonction', 'analyseIndex');
+
+        return $this->render('AeagSqeBundle:Programmation:Avancement\analyseIndex.html.twig');
+    }
+
+    public function analyseGlobalAction() {
+
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->render('AeagSqeBundle:Default:interdit.html.twig');
+        }
+        $session = $this->get('session');
+        $session->set('menu', 'programmation');
+        $session->set('controller', 'ProgrammationAvancement');
+        $session->set('fonction', 'analyseGlobal');
+        $em = $this->get('doctrine')->getManager();
+        $emSqe = $this->get('doctrine')->getManager('sqe');
+
+        $repoPgProgMarche = $emSqe->getRepository('AeagSqeBundle:PgProgMarche');
+
+        $tableau = $repoPgProgMarche->getAvancementAnalyseGlobal();
+
+
+//          \Symfony\Component\VarDumper\VarDumper::dump($tableau);
+//          return new Response (''); 
+
+        return $this->render('AeagSqeBundle:Programmation:Avancement\analyseGlobal.html.twig', array(
+                    'tableau' => $tableau,
+        ));
+    }
+
+    public function analysePeriodeAction() {
+
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->render('AeagSqeBundle:Default:interdit.html.twig');
+        }
+        $session = $this->get('session');
+        $session->set('menu', 'programmation');
+        $session->set('controller', 'ProgrammationAvancement');
+        $session->set('fonction', 'analysePeriode');
+        $em = $this->get('doctrine')->getManager();
+        $emSqe = $this->get('doctrine')->getManager('sqe');
+
+        $repoPgProgMarche = $emSqe->getRepository('AeagSqeBundle:PgProgMarche');
+
+        $tableau = $repoPgProgMarche->getAvancementAnalysePeriode();
+
+
+//          \Symfony\Component\VarDumper\VarDumper::dump($tableau);
+//          return new Response (''); 
+
+        return $this->render('AeagSqeBundle:Programmation:Avancement\analysePeriode.html.twig', array(
+                    'tableau' => $tableau,
+        ));
+    }
+    
+    public function analyseLotAction() {
+
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->render('AeagSqeBundle:Default:interdit.html.twig');
+        }
+        $session = $this->get('session');
+        $session->set('menu', 'programmation');
+        $session->set('controller', 'ProgrammationAvancement');
+        $session->set('fonction', 'analyseLotPrestataire');
+        $em = $this->get('doctrine')->getManager();
+        $emSqe = $this->get('doctrine')->getManager('sqe');
+
+        $repoPgProgMarche = $emSqe->getRepository('AeagSqeBundle:PgProgMarche');
+
+        $tableau = $repoPgProgMarche->getAvancementAnalyseLot();
+
+
+//          \Symfony\Component\VarDumper\VarDumper::dump($tableau);
+//          return new Response (''); 
+
+        return $this->render('AeagSqeBundle:Programmation:Avancement\analyseLot.html.twig', array(
+                    'tableau' => $tableau,
+        ));
+    }
+
 }
--- src/Aeag/SqeBundle/Repository/PgProgMarcheRepository.php
+++ src/Aeag/SqeBundle/Repository/PgProgMarcheRepository.php
@@ -73,98 +73,67 @@ class PgProgMarcheRepository extends EntityRepository {
      /**
      * @return array
      */
-    public function getAvancementGlobal() {
-        
-        $query = "select b.typeMarche,";
-        $query = $query . " a.nb_prel, b.nb_prel_prog,";
-        $query = $query . " case when a.statut_prel is null then 'Aucun suivi'";
-        $query = $query . " when a.statut_prel ='F' then 'Effectué sans FT'";
-        $query = $query . " when a.statut_prel ='D' then 'Effectué avec FT'";
-        $query = $query . " when a.statut_prel ='N' then 'Non effectué'";
-        $query = $query . " when a.statut_prel ='P' then 'Prévisionnel'";
-        $query = $query . " when a.statut_prel ='R' then 'Reporté'";
-        $query = $query . " end  statutPrel"; 
-        $query = $query . " from ";
-        $query = $query . " (select typeMarche,  count(prl.id)  nb_prel, sui.statutPrel";
-        $query = $query . " from Aeag\SqeBundle\Entity\PgCmdPrelev prl";
-        $query = $query . " left join (select id, prelev,"; 
-        $query = $query . " case when statutPrel = 'F' and fichierRps_id is not null then 'D'";
-        $query = $query . " else statutPrel end  statutPrel from Aeag\SqeBundle\Entity\PgCmdSuiviPrel) sui with prl.id = sui.prelev";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgCmdDemande dmd with dmd.id = prl.demande";
-        $query = $query . "  join Aeag\SqeBundle\Entity\PgProgLotAn lan with lan.id = dmd.lotan";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgProgLot lot with lot.id = lan.lot";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgProgMarche ma with ma.id = lot.marche";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgRefCorresPresta presta with presta.adrCorId = dmd.prestataire";
-        $query = $query . "  join Aeag\SqeBundle\Entity\PgRefStation_mesure sta with sta.ouvFoncId = prl.station";
-        $query = $query . " where (lot.codeMilieu in ('RHB','RHM')) ";
-        $query = $query . " and ((sui.id is null) or (";
-        $query = $query . " sui.id = (select max(ss.id) from Aeag\SqeBundle\Entity\PgCmdSuiviPrel ss where ss.prelev = prl.id)))";
-        $query = $query . " group by typeMarche,sui.statutPrel";
-        $query = $query . " ) a";
-        $query = $query . " join";
-        $query = $query . " (select typeMarche, count(prl.id)  nb_prel_prog";
-        $query = $query . " from Aeag\SqeBundle\Entity\PgCmdPrelev prl";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgCmdDemande dmd with dmd.id = prl.demande";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgProgLotAn lan with lan.id = dmd.lotan";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgProgLot lot with lot.id = lan.lot";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgProgMarche ma with ma.id = lot.marche";
-        $query = $query . " where (lot.codeMilieu in ('RHB','RHM'))";
-        $query = $query . " group by typeMarche";
-        $query = $query . " ) b with a.typeMarche = b.typeMarche";
-        $query = $query . " order by b.typeMarche, a.statutPrel";
-        
-        $qb = $this->_em->createQuery($query);
-        print_r($query);
-        return $qb->getResult();
+    public function getAvancementHydrobioGlobal() {
+   
+        $query = "select * from sqe_avancement_hydrobio_global() "; 
+         return $this->getEntityManager('')
+                ->getConnection()
+                ->query($query);
     }
     
-    /**
+      /**
      * @return array
      */
-    public function getAvancementGlobal1() {
-        
-         $query =  " select ma.typeMarche,  nullif(count(prl.id),0)  nb_prel,";
-         $query = $query . " case when sui.statutPrel = 'F' and coalesce(max(sui.fichierRps), 0)  <> 0 then 'D'  else sui.statutPrel end statutPrel";
-        $query = $query . " from Aeag\SqeBundle\Entity\PgCmdPrelev prl";
-        $query = $query . "  join Aeag\SqeBundle\Entity\PgCmdSuiviPrel  sui with prl.id = sui.prelev"; 
-         $query = $query . " join Aeag\SqeBundle\Entity\PgCmdDemande dmd with dmd.id = prl.demande";
-        $query = $query . "  join Aeag\SqeBundle\Entity\PgProgLotAn lan with lan.id = dmd.lotan";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgProgLot lot with lot.id = lan.lot";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgProgMarche ma with ma.id = lot.marche";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgRefCorresPresta presta with presta.adrCorId = dmd.prestataire";
-        $query = $query . "  join Aeag\SqeBundle\Entity\PgRefStationMesure sta with sta.ouvFoncId = prl.station";
-        $query = $query . " where (lot.codeMilieu in ('RHB','RHM')) ";
-        $query = $query . " and ((sui.id is null) or (";
-        $query = $query . " sui.id = (select max(ss.id) from Aeag\SqeBundle\Entity\PgCmdSuiviPrel ss where ss.prelev = prl.id)";
-        $query = $query . " )";
-        $query = $query . " )"; 
-        $query = $query . " group by ma.typeMarche,sui.statutPrel";
-        $query = $query . " order by ma.typeMarche, sui.statutPrel";
-        
-        $qb = $this->_em->createQuery($query);
-        //print_r($query);
-        return $qb->getResult();
+    public function getAvancementHydrobioSupport() {
+   
+        $query = "select * from sqe_avancement_hydrobio_support() "; 
+         return $this->getEntityManager('')
+                ->getConnection()
+                ->query($query);
+    }
+    
+      /**
+     * @return array
+     */
+    public function getAvancementHydrobioLot() {
+   
+        $query = "select * from sqe_avancement_hydrobio_lot() "; 
+         return $this->getEntityManager('')
+                ->getConnection()
+                ->query($query);
     }
     
      /**
      * @return array
      */
-    public function getAvancementGlobal2() {
-        
-         $query =  " select ma.typeMarche,  nullif(count(prl.id),0)  nb_prel";
-        $query = $query . " from Aeag\SqeBundle\Entity\PgCmdPrelev prl";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgCmdDemande dmd with dmd.id = prl.demande";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgProgLotAn lan with lan.id = dmd.lotan";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgProgLot lot with lot.id = lan.lot";
-        $query = $query . " join Aeag\SqeBundle\Entity\PgProgMarche ma with ma.id = lot.marche";
-        $query = $query . " where (lot.codeMilieu in ('RHB','RHM'))";
-        $query = $query . " group by ma.typeMarche";
-        $query = $query . " order by ma.typeMarche";
-        
-        $qb = $this->_em->createQuery($query);
-        //print_r($query);
-        return $qb->getResult();
+    public function getAvancementAnalyseGlobal() {
+   
+        $query = "select * from sqe_avancement_analyse_global() "; 
+         return $this->getEntityManager('')
+                ->getConnection()
+                ->query($query);
     }
-
+    
+     /**
+     * @return array
+     */
+    public function getAvancementAnalysePeriode() {
+   
+        $query = "select * from sqe_avancement_analyse_periode() "; 
+         return $this->getEntityManager('')
+                ->getConnection()
+                ->query($query);
+    }
+    
+     /**
+     * @return array
+     */
+    public function getAvancementAnalyseLot() {
+   
+        $query = "select * from sqe_avancement_analyse_lot() "; 
+         return $this->getEntityManager('')
+                ->getConnection()
+                ->query($query);
+    }
 
 }
